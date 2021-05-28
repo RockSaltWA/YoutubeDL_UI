e9c4f6fb594f9bff452c0ad8f21cfa73f27f7aca
WollywogZ <jack.doughty11@gmail.com>
2021-05-28 11:09:52 -0700

    video_display.py scrapped

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 from pytube import YouTube
+import video_display
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.bad_chars = ['/', "\\", ':', '*', '?', '"', '<', '>', '|', "'", ".", "~"]
+
         self.master = master
+
         self.pack()
         self.create_widgets()
 
@@ -20,7 +24,7 @@ class Application(tk.Frame):
 
         self.uwu = tk.Button(self)
         self.uwu["text"] = "Download"
-        self.uwu["command"] = self.hentai_download
+        self.uwu["command"] = self.mp4_download
         self.uwu.grid(row=1, column=2)
 
         self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
@@ -31,14 +35,9 @@ class Application(tk.Frame):
         # self.button_bonus.grid(columnspan=3)
 
     def download_popup(self, title):
-        win = tk.Toplevel()
-        win.wm_title("Downloading %s", title)
-
-        # l = tk.Label(win, text="Input")
-        # l.grid(row=0, column=0)
-        #
-        # b = tk.Button(win, text="Okay", command=win.destroy)
-        # b.grid(row=1, column=0)
+        # win = tk.Toplevel()
+        # win.wm_title("Downloading %s", title)
+        pass
 
     def kaboom_alphabet(self):
         alphabet = []
@@ -48,20 +47,37 @@ class Application(tk.Frame):
             alphabet.append(chr(i))
         return alphabet
 
-    def hentai_download(self):
+    def mp4_download(self):
         alphabet = self.kaboom_alphabet
-        url = self.vid_url.get()
-        yt = YouTube(url)
+        yt = YouTube(self.vid_url.get())
         title = yt.title
+        for i in self.bad_chars:
+            title = title.replace(i, '')
+        print(title)
         self.download_popup(title)
+        print(title)
         yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+        print(title)
+        video_display.MyVideoCapture("./" + title + ".mp4")
+        print(title)
+
+    # def mp3_download(self):
+    #     alphabet = self.kaboom_alphabet
+    #     url = self.vid_url.get()
+    #     yt = YouTube(url)
+    #     title = yt.title
+    #     self.download_popup(title)
+    #     yt.streams.filter(progressive=True, file_extension='mp3').order_by('resolution').desc().first().download()
+
 
 root = tk.Tk()
 root.title("YouTube Downloader")
 # root.geometry("852x480")
-#loading image logo
-root.iconphoto(True, tk.PhotoImage(file ='eyt.png'))
-#Resizeable for different monitors
+
+# loading image logo
+root.iconphoto(True, tk.PhotoImage(file='eyt.png'))
+
+# Resizeable for different monitors
 root.resizable(True, True)
 app = Application(master=root)
 app.mainloop()
